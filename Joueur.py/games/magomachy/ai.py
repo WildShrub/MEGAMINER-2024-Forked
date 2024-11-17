55bd6801840ab29c4f1effeb815a48eeef6f95bd
WildShrub <erichampson825@gmail.com>
2024-11-17 12:22:16 -0600

    Update ai.py

--- Joueur.py/games/magomachy/ai.py
+++ Joueur.py/games/magomachy/ai.py
@@ -129,7 +129,8 @@ class AI(BaseAI):
                         tile = self.player.wizard.tile.tile_east
                     if self.player.wizard.aether > 3 :
                         
-                        self.player.wizard.cast('cast', {'spellName': "Calming Blast",'tile': (Tile) self.player.opponent.wizard.tile})
+                        self.player.wizard.cast("Calming Blast",self.player.opponent.wizard.tile)
+                        
 
             
                        
